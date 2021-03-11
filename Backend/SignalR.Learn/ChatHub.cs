d4727ad59ebf92155613f4ff36637bab49c17395
puliwang@cxist.com <puliwang@cxist.com>
2021-03-11 09:44:47 +0800

    2

--- Backend/SignalR.Learn/ChatHub.cs
+++ Backend/SignalR.Learn/ChatHub.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
